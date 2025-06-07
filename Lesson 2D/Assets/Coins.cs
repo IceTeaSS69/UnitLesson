d9426ad1cef8a1c2b94c825e6d2087c3e87cbfab
IceTeaSS69 <dima.kuzin.1010@gmail.com>
2025-06-07 20:50:44 +0300

    Added A-Player and Coins

---
+++ Lesson 2D/Assets/Coins.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework.Internal.Commands;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Coins : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void OnCollisionEnter2D (Collision2D Collision)
+    {
+        if (Collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
